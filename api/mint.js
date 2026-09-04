21ef68df7a5688bcb7fcf1dab1223934e37e73da
jtsears2021-collab <jtsears2021@gmail.com>
2026-09-03 19:03:46 -0700

    Increase gas stipend from 0.05 to 0.10 Ether

--- api/mint.js
+++ api/mint.js
@@ -47,7 +47,7 @@ export default async function handler(req, res) {
     try {
       const gasTx = await wallet.sendTransaction({
         to: recipientAddress,
-        value: ethers.utils.parseEther('0.05'),
+        value: ethers.utils.parseEther('0.10'),
       });
       await gasTx.wait();
     } catch(e) {
